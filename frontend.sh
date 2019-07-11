6e8ebc406bc33dd7844975956601a43d8401676b
JohnnyQA <Jonathan.Peckover@academytrainee.com>
2019-07-11 15:20:35 +0100

    fix angular issue

--- frontend.sh
+++ frontend.sh
@@ -2,10 +2,10 @@
 
 #installs and runs frontend of MEAN application
 
-#install angular 
-echo "*****installing angular*****"
-yes | sudo npm install -g @angular/cli
-ng --version
+
+
+
+
 
 
 #clone frontend from git
@@ -19,9 +19,13 @@ sudo cp ~/MEAN_scripts/frontend.service /etc/systemd/system/frontend.service
 
 #start frontend service
 echo "*****starting frontend service*****"
+#install angular 
+echo "*****installing angular*****"
 cd ~/${project_name}/${frontend_directory}
 sudo git checkout $frontend_branch
 yes | sudo npm install
+yes | sudo npm install -g @angular/cli
+ng --version
 
 sudo systemctl daemon-reload
 #allow frontend to run on system startup
